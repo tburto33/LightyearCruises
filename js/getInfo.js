ad32e0acf9b6046e36f24e193360dc2b627bfc0b
Alex Burton <tburto2387@gmail.com>
2021-02-28 14:24:03 -0500

    started working on html form

---
+++ js/getInfo.js
@@ -0,0 +1,7 @@
+function getInfo(){
+    var firstName = document.getElementById("firstName");
+    var lastName = document.getElementById("lastName");
+    var email = document.getElementById("email");
+
+    alert(`Hello, ${firstName} ${lastName}. Your email is ${email}.`);
+}
