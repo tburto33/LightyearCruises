a4f282afa5b549de095b640ccbaeef82ec505a9f
tburto33 <tburto2387@gmail.com>
2021-02-28 17:47:39 -0500

    form now sends first/last name and email to console.

--- js/getInfo.js
+++ js/getInfo.js
@@ -1,7 +1,36 @@
-function getInfo(){
-    var firstName = document.getElementById("firstName");
-    var lastName = document.getElementById("lastName");
-    var email = document.getElementById("email");
+let contactInfo = [];
 
-    alert(`Hello, ${firstName} ${lastName}. Your email is ${email}.`);
+const getContactInfo = (ev)=>{
+    ev.preventDefault();
+    let info = {
+        firstName: document.getElementById("firstName").value,
+        lastName: document.getElementById("lastName").value,
+        email: document.getElementById("email").value
+    }
+    contactInfo.push(info);
+    document.forms[0].reset(); //resets form
+
+    if(info.firstName == "" || info.lastName == "" || info.email == ""){
+        console.log("Missing information");
+    }else {
+        console.log(`Hello, ${info.firstName} ${info.lastName}. Your email is ${info.email}.`);
+    }    
+}
+document.addEventListener('DOMContentLoaded', ()=>{
+    document.getElementById('btn').addEventListener('click', getContactInfo);
+})
+
+let totalCruiseSum = 0;
+var extrasSpaceWalk = document.getElementById("spacewalk");
+var extrasZeroGravity = document.getElementById("zero-gravity");
+var extrasShuttle = document.getElementById("shuttle");
+var extrasVIP = document.getElementById("vip");
+
+const cruiseExtras = ()=>{
+    let extrasPricing = {
+        spacewalk: 500,
+        zeroGravity: 100,
+        shuttle: 1000,
+        vip: 2000
+    }
 }
