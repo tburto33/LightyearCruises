2425268a9a9adb3f3b9b9cc7e9e1063bfbb0dc00
tburto33 <tburto2387@gmail.com>
2021-03-09 18:35:29 -0500

    buttons

--- js/getInfo.js
+++ js/getInfo.js
@@ -7,12 +7,15 @@ var formatter = new Intl.NumberFormat('en-US', {
 //checks for destination & extras returns total of values
 const getCruiseCost = (e) => {
     e.preventDefault();
+    disPayBtn();
     var checkedVal = []; //temp array for stored values
+    var btnDisable = document.getElementById('btn-pay')
 
     //validates destination & pushes value of checked to array
     let dest = document.querySelector('input[name=planet]:checked');
     if(dest){
         checkedVal.push(parseInt(dest.value));
+        enPayBtn();
     }else{
         alert("Please select a destination!");
     }
@@ -25,16 +28,23 @@ const getCruiseCost = (e) => {
         }
     }
 
+    //adds all extras and dest pushed to checkedVal[]
     let tripTotal = formatter.format(checkedVal.reduce((a,b) => a + b, 0));
 
     //adds values from array and returns total to form.html
     document.getElementById('tripTotal').innerHTML = `${tripTotal}`;
 }
 
-//listener for submit btn
+//listener for Est Total btn on destform.html
 document.addEventListener('DOMContentLoaded', ()=>{
     document.getElementById('btn').addEventListener('click', getCruiseCost);
 })
 
+function disPayBtn(){
+    document.getElementById('btn-pay').disabled;
+}
 
+function enPayBtn(){
+    document.getElementById('btn-pay').disabled = false;
+}
 
